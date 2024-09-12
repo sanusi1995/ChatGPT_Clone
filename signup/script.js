432059707bdfda3ea9b085f0d25f239eded6f2f5
Towobola <towobolak@gmail.com>
2024-09-12 11:42:34 +0100

    signup commit

---
+++ signup/script.js
@@ -0,0 +1,9 @@
+const header1 = document.getElementById('header1');
+const header3 = document.getElementById('header3');
+const password = document.getElementById('password');
+
+let passsword = function(){
+    header3.style.display = 'block';
+    password.style.display = 'block';
+    header1.style.display = 'none';
+}
